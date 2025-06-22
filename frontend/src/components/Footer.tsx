b26eb1472a5fb33456616fdaec5e3264b4e765bb
Shrinivas Sherikar <ssherikar2005@gmail.com>
2025-06-22 20:14:45 +0530

    layout fixes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,15 +2,13 @@ import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div className='w-full p-5 bg-[#141414] grid lg:grid-cols-3  grid-cols-2 grid-rows-2 gap-5'>
-      
- 
-      <div className='text-[#666666] text-xl font-semibold text-center md:text-center lg:text-start '>
+    <div className='w-full  bg-[#141414] grid lg:grid-cols-3 lg:grid-rows-1 grid-cols-2 grid-rows-2 gap-5 p-5'>
+      <div className='text-[#666666]  text-xl font-semibold text-center md:text-center lg:text-start '>
         WriteFlow
       </div>
 
    
-      <div className='flex gap-3 justify-center items-center order'>
+      <div className='flex gap-3 justify-center items-center order '>
         <FaInstagram size={20} className=' cursor-pointer' />
         <FaLinkedin size={20} className=' cursor-pointer' />
         <FaGithub size={20} className=' cursor-pointer' />
@@ -20,6 +18,8 @@ function Footer() {
       <p className='text-[#666666] text-sm text-center md:text-center lg:text-end col-span-2 lg:col-span-1 '>
         © 2024 FutureTech. All rights reserved.
       </p>
+ 
+      
     </div>
   )
 }
